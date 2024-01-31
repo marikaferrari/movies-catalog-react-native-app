29e20a7cc9a36aa457b6e4ae6daa9a00085ead0d
Marika <ferrari.marika@gmail.com>
2024-01-31 17:08:59 +0000

    Nativewind styling bug fixed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, TouchableOpacity, ScrollView, Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import { StatusBar } from 'expo-status-bar';
 
 export default function HomeScreen() {
+  const ios = Platform.OS === 'ios';
   return (
-    <SafeAreaView>
-      <Text>Hello</Text>
-    </SafeAreaView>
+    <View className="flex-1 bg-black">
+      {/* search bar and logo */}
+      {/* the following margin setting won't work on Android  */}
+      <SafeAreaView className={ios ? '-mb-2' : 'mb-3'}>
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+          <Text className="text-white text-3xl font-bold">Hello</Text>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 }
