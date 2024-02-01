43a71afe6da00091afe614bce9aa211c01c593ab
Marika <ferrari.marika@gmail.com>
2024-02-01 16:57:25 +0000

    Initial Setup for PersonScreen

---
+++ screens/PersonScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { ViewPropTypes } from 'deprecated-react-native-prop-types';
+
+export default function PersonScreen() {
+  return (
+    <ViewPropTypes>
+      <Text>Person</Text>
+    </ViewPropTypes>
+  );
+}
