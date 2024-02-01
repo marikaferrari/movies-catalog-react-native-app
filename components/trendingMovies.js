f6cabaf97c7f813d4bf81713c25eb7dba59ad38b
Marika <ferrari.marika@gmail.com>
2024-02-01 14:21:02 +0000

    Fixed issue NativeWind with components

--- components/trendingMovies.js
+++ components/trendingMovies.js
@@ -1,1 +1,12 @@
-export default function TrendingMovies({ data }) {}
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function TrendingMovies() {
+  return (
+    <View style={{ marginBottom: 8, marginTop: 20 }}>
+      <Text style={{ color: 'white', fontSize: 20, marginLeft: 4, marginBottom: 5 }}>
+        Trending Movies
+      </Text>
+    </View>
+  );
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,12 +6,16 @@ import { StatusBar } from 'expo-status-bar';
 import { styles } from '../theme';
 import TrendingMovies from '../components/trendingMovies';
 
+// declare variable to fix styling between platforms
+const ios = Platform.OS === 'ios';
+
 export default function HomeScreen() {
-  const ios = Platform.OS === 'ios';
+  // state for changing trending movies
+  const [trending, setTrending] = useState([1, 2, 3]);
+
   return (
     <View className="flex-1 bg-black">
       {/* search bar and logo */}
-      {/* the following margin setting won't work on Android  */}
       <SafeAreaView className={ios ? '-mb-2' : 'mb-3'}>
         <StatusBar style="light" />
         <View className="flex-row justify-between items-center mx-4">
@@ -30,7 +34,7 @@ export default function HomeScreen() {
         contentContainerStyle={{ paddingBottom: 10 }}
       >
         {/* trending movies carousel */}
-        <TrendingMovies data={''} />
+        <TrendingMovies data={trending} />
       </ScrollView>
     </View>
   );
